1f175aa11125e5293882c9da832594d67dfce938
Skye Chen <zyxwvugaming@gmail.com>
2022-03-05 17:26:29 -0500

    Update Bot_Offline_Message.py

--- Scripts/Bot_Offline_Message.py
+++ Scripts/Bot_Offline_Message.py
@@ -6,6 +6,7 @@ bot_config = {
     'sr': 'TheOwlHouse',                                           # str: the subreddit being monitored
     'bot_status_post_id': 'svj2yd',           # str: Reddit id of the bot status post
     'replies_enabled': False,                      # bool: if true, allows the bot to reply to msgs
+    'status': ''
 }
 
 api.edit_status(bot_config, False)
